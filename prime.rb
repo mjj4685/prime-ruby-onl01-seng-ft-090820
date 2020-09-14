c63f3767b28d5f5de2bb29c03ccfa98057b44c83
MJ Johnson <mjohnson4685@gmail.com>
2020-09-14 04:54:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  i = 2
+  while i <= number / 2
+    if number % i == 0
+      return false
+    end
+    i += 1
+  end
+  return true
+end
+
+puts prime?(31)
